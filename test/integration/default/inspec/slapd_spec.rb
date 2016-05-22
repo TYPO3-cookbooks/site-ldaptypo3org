6d4ecbbeedb78e27a26d55b5aeeaede235f63c1e
Steffen Gebert <steffen.gebert@typo3.org>
2016-05-22 11:41:56 +0200

    Remove some tests that don't work with kitchen-docker

--- test/integration/default/inspec/slapd_spec.rb
+++ test/integration/default/inspec/slapd_spec.rb
@@ -7,14 +7,15 @@ control 'slapd-1' do
     it { should be_installed }
   end
 
-  describe service('slapd') do
-    it { should be_running }
-  end
+  #describe service('slapd') do
+  #  it { should be_installed }
+  #  it { should be_running }
+  #end
 
   describe port(389) do
     it { should be_listening }
     its('protocols') { should include 'tcp'}
     its('protocols') { should include 'tcp6'}
-    its('processes') { should include 'slapd' }
+    # its('processes') { should include 'slapd' }
   end
 end
