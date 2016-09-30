2e0b24514da2af925874fa1afd67351fc97c4fa2
Michael Stucki <michael.stucki@typo3.org>
2016-09-30 15:08:42 +0200

    [BUGFIX] Fix order of attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,8 @@ default['openldap']['schemas'] = %w(core.schema cosine.schema inetorgperson.sche
 default['openldap']['basedn'] = 'o=typo3'
 
 # SSL
+default['site-ldaptypo3org']['ssl_certificate'] = 'wildcard.typo3.org'
 default['openldap']['manage_ssl'] = false
 default['openldap']['ssl_cert'] = File.join(node['ssl_certificates']['path'], node['site-ldaptypo3org']['ssl_certificate']) + ".crt"
 default['openldap']['ssl_key']  = File.join(node['ssl_certificates']['path'], node['site-ldaptypo3org']['ssl_certificate']) + ".key"
 default['openldap']['cafile']   = File.join(node['ssl_certificates']['path'], node['site-ldaptypo3org']['ssl_certificate']) + ".ca-bundle"
-default['site-ldaptypo3org']['ssl_certificate'] = 'wildcard.typo3.org'
